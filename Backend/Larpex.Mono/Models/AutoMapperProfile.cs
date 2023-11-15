004feb42e4543c44f2abe97dc2cb10af853f3c15
Mikołaj <mrefe12345@gmail.com>
2023-11-15 13:36:23 +0100

    [Backend] added not implemented event functionality

---
+++ Backend/Larpex.Mono/Models/AutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Larpex.Shared.ModelDto;
+
+namespace Larpex.Mono.Models;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<Event, EventDto>();
+        CreateMap<EventDto, Event>();
+    }
+}
--- Backend/Larpex.Mono/Program.cs
+++ Backend/Larpex.Mono/Program.cs
@@ -1,3 +1,8 @@
+using Larpex.Mono.Extensions;
+using Larpex.Mono.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<LarpexDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Dependency injection services here
+builder.Services.AddTransients();
 
 var app = builder.Build();
 
