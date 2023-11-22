fdc5f537fa2ade50175e703fc9a16da0e06ed7d8
Kuba Wilk <kuba99p@gmail.com>
2023-11-22 22:23:38 +0100

    changes

--- Frontend/src/entities/EventDto.ts
+++ Frontend/src/entities/EventDto.ts
@@ -3,6 +3,12 @@ interface EventDto {
   eventName: string;
   eventStatus: string;
   eventDescription: string;
+  game: string;
+  payment: string;
+  location: string;
+  date: string;
+  peopleCount: number;
+  img: string;
 }
 
 export default EventDto;
