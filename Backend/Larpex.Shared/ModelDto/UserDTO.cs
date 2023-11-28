88ba7f7cd26a9b39a32b4c3efef879fd5fae060c
Mikołaj <mrefe12345@gmail.com>
2023-11-28 17:17:29 +0100

    user snippet

---
+++ Backend/Larpex.Shared/ModelDto/UserDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Larpex.Shared.ModelDto;
+
+public class UserDTO
+{
+    public int UserId { get; set; }
+
+    public string UserUsername { get; set; } = null!;
+
+    public string UserEmail { get; set; } = null!;
+
+    public string? UserFirstName { get; set; }
+
+    public string? UserLastName { get; set; }
+
+    public DateTime? UserBirthDay { get; set; }
+}
