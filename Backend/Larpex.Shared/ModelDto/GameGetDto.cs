4e22e0a7ec11eb735b4447f619744e83a6fc42c3
Rafał Sulowski <01159502@pw.edu.pl>
2023-12-04 12:18:04 +0100

    returned value form getGame, greGames, GameDto -> GameGetDto

---
+++ Backend/Larpex.Shared/ModelDto/GameGetDto.cs
@@ -0,0 +1,20 @@
+﻿using Larpex.Shared.ModelDto;
+
+namespace Larpex.Mono.Models;
+
+public class GameGetDto
+{
+    public int GameId { get; set; }
+
+    public string GameName { get; set; } = null!;
+
+    public string GameAuthor { get; set; } = null!;
+
+    public string? GameDescription { get; set; }
+
+    public string? GameScript { get; set; }
+
+    public int? GameDifficulty { get; set; }
+
+    public int? GameMaxNumberOfParticipants { get; set; }
+}
