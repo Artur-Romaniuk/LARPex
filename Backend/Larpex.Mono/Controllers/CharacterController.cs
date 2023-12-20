78d5755e25c26f4c3ad9a6bd92c2faddd159ef76
Rafał Sulowski <01159502@pw.edu.pl>
2023-12-20 14:40:16 +0100

    characters list added to GetGameDto

---
+++ Backend/Larpex.Mono/Controllers/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using Larpex.Mono.Repositories.Interfaces;
+using Larpex.Mono.Services.Interfaces;
+using Larpex.Shared.ModelDto;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Larpex.Mono.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CharacterController : ControllerBase
+{
+    private readonly ICharacterService _CharacterService;
+
+    public CharacterController(
+        ICharacterService CharacterService
+        )
+    {
+        _CharacterService = CharacterService;
+    }
+
+    [HttpGet("{gameId}")]
+    [ProducesResponseType(typeof(List<GameCharacterDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<List<GameCharacterDto>>> GetGameCharacters(int gameId)
+    {
+        var Character = await _CharacterService.GetGameCharacters(gameId);
+        return Ok(Character);
+    }
+}
